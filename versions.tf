c12350a76f6756c4c55b4081154d3d0e368a901b
kalenarndt <kalenarndt@gmail.com>
2023-07-24 09:24:49 -0600

    rtx and ver changes

--- versions.tf
+++ versions.tf
@@ -1,14 +1,3 @@
-## Place your Terraform Args / Provider version args here
-/* terraform {
-  required_version = ">=1.3.0"
-  required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "~> 3.11.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.4.3"
-    }
-  }
-} */
+terraform {
+  required_version = ">=1.4.0"
+}
